5293754640d887414a2d14e9dd2b78d8ca393f05
dr3w <mail2dr3w@gmail.com>
2017-04-28 17:17:14 +0300

    Update fetch api; support REST api

--- src/articles/actions.js
+++ src/articles/actions.js
@@ -2,8 +2,9 @@ import { GET_ALL_ARTICLES } from '../common/constants'
 
 const getAllArticles = () => ({
   type: GET_ALL_ARTICLES,
-  api: '/api/article',
-  method: 'GET'
+  api: {
+    url: '/api/article'
+  }
 })
 
 export { getAllArticles }
--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -1,41 +1,57 @@
 import fetch from 'isomorphic-fetch'
+import qs from 'qs'
 import { START, SUCCESS, FAIL } from '../common/constants'
 
+const headers = new Headers({
+  'Content-Type': 'application/json'
+})
+
+const getRequestBody = (method, data) => (
+  (!(method === 'GET' || method === 'HEAD') && data && JSON.stringify(data)) || null
+)
+
+const getQueryParams = (method, data) => (
+  ((method === 'GET' || method === 'HEAD') && data && `?${qs.stringify(data)}`) || ''
+)
+
+const onFetch = response => (
+  new Promise((resolve, reject) => {
+    if (response.status >= 200 && response.status < 300) {
+      resolve(response.json())
+    } else {
+      const error = new Error(response.statusText || response.status)
+      error.response = response
+
+      reject(error)
+    }
+  })
+)
+
 export default (/* store */) => next => (action) => {
-  const { api, type, method, ...rest } = action
+  const { api, type, ...rest } = action
 
   if (!api) return next(action)
 
-  const request = new Request(api, {
-    method: method || 'GET',
-    redirect: 'follow',
-    headers: new Headers({
-      'Content-Type': 'text/plain'
-    })
-  })
-
-  const onFetch = response => (
-    new Promise((resolve, reject) => {
-      if (response.status >= 200 && response.status < 300) {
-        resolve(response.json())
-      } else {
-        const error = new Error(response.statusText || response.status)
-
-        error.response = response
-        reject(error)
-      }
-    })
-  )
-
   const onSuccess = response => next({ ...rest, type: type + SUCCESS, response })
   const onFail = error => next({ ...rest, type: type + FAIL, error })
+  const doFetch = ({ url, method = 'GET', data }) => {
+    const body = getRequestBody(method, data)
+    const queryUrl = url + getQueryParams(method, data)
 
-  setTimeout(() => {
-    fetch(request)
-      .then(onFetch)
-      .then(onSuccess)
-      .catch(onFail)
-  }, 1000)
+    const request = new Request(queryUrl, {
+      method, body, headers
+    })
+
+    // TODO: remove
+    setTimeout(() => {
+      fetch(request)
+        .then(onFetch)
+        .then(onSuccess)
+        .catch(onFail)
+    }, 1000)
+  }
+
+  doFetch(api)
 
   return next({ ...rest, type: type + START })
 }
