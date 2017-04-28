3cde6805ff201fb427e2e705fb5b1cf0cc836a97
dr3w <mail2dr3w@gmail.com>
2017-04-28 11:48:40 +0300

    Fix logger middleware

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 export default store => next => (action) => {
-  console.debug('before', store.getState())
-  console.debug('---', 'dipatching', action)
+  console.debug('STORE', 'dipatching', action)
+  console.debug('STORE', 'before', Object.assign({}, store.getState()))
   next(action)
-  console.debug('after', store.getState())
+  console.debug('STORE', 'after', Object.assign({}, store.getState()))
 }
 /* eslint-enable no-console */
 
