86a665787c4662e1dcd9dcd4cfa5444d8ab64cb7
dr3w <mail2dr3w@gmail.com>
2017-05-02 19:50:22 +0300

    Refactor ArticlesId component loading

--- src/app/articles/id/ArticlesId.jsx
+++ src/app/articles/id/ArticlesId.jsx
@@ -3,17 +3,19 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getArticle } from 'app/articles/id/actions'
 import { Status } from 'common/components'
-import { LOADED, PRISTINE } from 'common/constants'
+import { LOADED } from 'common/constants'
+
+const load = (props) => {
+  const { match, article } = props
+  return !article.id && props.getArticle(match.params.id)
+}
 
 class ArticlesId extends React.PureComponent {
-  componentWillReceiveProps(nextProps) {
-    // TODO: fix check, first loading does not work
-    if (nextProps.status !== PRISTINE && nextProps.match.params.id === this.props.match.params.id) {
-      return
-    }
-
-    nextProps.getArticle(nextProps.match.params.id)
+  componentDidMount() {
+    load(this.props)
   }
+
+  componentWillReceiveProps = load
 
   render() {
     const { article, status, error } = this.props
