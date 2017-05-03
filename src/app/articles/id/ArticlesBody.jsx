cb77eeb3aaf8593ed74d11cc43ca26b43127a697
dr3w <mail2dr3w@gmail.com>
2017-05-03 15:02:40 +0300

    Rename comment component

--- src/app/articles/id/ArticlesBody.jsx
+++ src/app/articles/id/ArticlesBody.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { CommentListPreload } from 'common/components'
+import { CommentListLazy } from 'common/components'
 
 const ArticleBody = ({ text, date, comments, loadComments }) => (
   <div>
     <p>{text}</p>
     <p><b>{date}</b></p>
 
-    <CommentListPreload comments={comments} loadComments={loadComments} />
+    <CommentListLazy comments={comments} loadComments={loadComments} />
   </div>
 )
 
--- src/common/components/comments/CommentListLazy.jsx
+++ src/common/components/comments/CommentListLazy.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Button, CommentList } from 'common/components'
 
-const CommentListPreload = ({ comments, loadComments }) => (
+const CommentListLazy = ({ comments, loadComments }) => (
   <div>
     { comments.length ?
       <CommentList comments={comments} /> : <Button text="Load comments" onClick={loadComments} /> }
   </div>
 )
 
-CommentListPreload.propTypes = {
+CommentListLazy.propTypes = {
   comments: PropTypes.array,
   loadComments: PropTypes.func
 }
 
-CommentListPreload.defaultProps = {
+CommentListLazy.defaultProps = {
   comments: [],
   loadComments: () => null
 }
 
-export default CommentListPreload
+export default CommentListLazy
