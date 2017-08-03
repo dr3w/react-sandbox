347343f828821112bccb564c3409bc719ffe8a80
dr3w <mail2dr3w@gmail.com>
2017-08-03 14:29:27 +0300

    Fix className

--- src/components/todo/Item.jsx
+++ src/components/todo/Item.jsx
@@ -2,34 +2,36 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import withToggle from 'hoc/withToggle'
 
-const TodoItem = ({ todoToggle, todoDelete, item, show, hide, toggledOn, isLoading, isError }) => (
-  <li
-    className={
-      `list-group-item todo-list-item
-      ${isLoading ? 'item-updating' : ''}
-      ${isError ? 'item-error' : ''}`
-    }
-    onMouseEnter={show}
-    onMouseLeave={hide}
-  >
-    <button
-      className={`btn btn-xs btn-danger ${toggledOn ? '' : 'hidden'}`}
-      onClick={!isLoading && todoDelete}
+const TodoItem = ({ todoToggle, todoDelete, item, show, hide, toggledOn, isLoading, isError }) => {
+  const liClassName = ['list-group-item', 'todo-list-item']
+  if (isLoading) liClassName.push('item-updating')
+  if (isError) liClassName.push('item-error')
+
+  return (
+    <li
+      className={liClassName.join(' ')}
+      onMouseEnter={show}
+      onMouseLeave={hide}
     >
-      DELETE
-    </button>
-    <div
-      htmlFor={item.id}
-      className="item-label clickable"
-      onClick={!isLoading && todoToggle}
-    >
-      <div className={`glyphicon checkmark ${item.isDone ? 'glyphicon-check' : 'glyphicon-unchecked'}`} />
-      <div className="item-text">
-        {item.text}
+      <button
+        className={`btn btn-xs btn-danger ${toggledOn ? '' : 'hidden'}`}
+        onClick={!isLoading && todoDelete}
+      >
+        DELETE
+      </button>
+      <div
+        htmlFor={item.id}
+        className="item-label clickable"
+        onClick={!isLoading && todoToggle}
+      >
+        <div className={`glyphicon checkmark ${item.isDone ? 'glyphicon-check' : 'glyphicon-unchecked'}`} />
+        <div className="item-text">
+          {item.text}
+        </div>
       </div>
-    </div>
-  </li>
-)
+    </li>
+  )
+}
 
 TodoItem.propTypes = {
   item: PropTypes.object,
